7dd639e5064b6432a5f0f6b264a86c79609bda3c
Mohsen Yazdani <imyazdani@gmail.com>
2024-11-29 19:06:57 +0330

    Write a test in multithreading situation

---
+++ src/main/java/org/example/creational/singleton/Main.java
@@ -0,0 +1,30 @@
+package org.example.creational.singleton;
+
+/*
+    If you see the same value, then singleton was reused.
+    If you see different values, then 2 singletons were created.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Thread threadFoo = new Thread(new ThreadFoo());
+        Thread threadBar = new Thread(new ThreadBar());
+        threadFoo.start();
+        threadBar.start();
+    }
+
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("FOO");
+            System.out.println(singleton.getValue());
+        }
+    }
+
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("BAR");
+            System.out.println(singleton.getValue());
+        }
+    }
+}
