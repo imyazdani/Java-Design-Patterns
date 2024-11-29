0aba1bd13ed725b4860465d36dfa1c6f856c355a
Mohsen Yazdani <imyazdani@gmail.com>
2024-11-29 19:06:14 +0330

    Make the object private in singleton class

--- src/main/java/org/example/creational/singleton/Singleton.java
+++ src/main/java/org/example/creational/singleton/Singleton.java
@@ -3,25 +3,31 @@ package org.example.creational.singleton;
 /*
     Singleton is a creational design pattern, which ensures that only one object of its kind exists
     and provides a single point of access to it for any other code.
+    This singleton class is Thread-Safe.
  */
 public class Singleton {
     private static volatile Singleton instance;
 
-    public String value;
+    private final String value;
 
     private Singleton(String value) {
         this.value = value;
     }
 
+    public String getValue() {
+        return value;
+    }
+
     public static Singleton getInstance(String value) {
-        Singleton result = instance;
-        if (result != null) {
-            return result;
+        if (instance != null) {
+            return instance;
         }
-        synchronized(Singleton.class) {
+
+        synchronized (Singleton.class) {
             if (instance == null) {
                 instance = new Singleton(value);
             }
+            System.out.println("An instance is created with value: " + value);
             return instance;
         }
     }
