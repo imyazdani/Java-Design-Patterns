6445ba292f8d05658f63955f268bc858d21d5932
Mohsen Yazdani <imyazdani@gmail.com>
2024-11-29 12:25:59 +0330

    initial mvn project

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MainTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
